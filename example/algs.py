dc9f6cefd80cfb5411a32944a83238d053a1bd6d
George Hart <ghartoularos@gmail.com>
2018-01-26 22:21:28 -0800

    updated example and test with tests

--- example/algs.py
+++ example/algs.py
@@ -6,44 +6,57 @@
 import numpy as np
 import random
 
-def isinputcool(seq,lo=None,hi=None):
+def isinputcool(seq,acount,ccount,inp,lo=None,hi=None):
     '''
     Function checks quality of input for both main functions before
     performing algorithms
     '''
     trivial = False
+    acount += 1
 
     # catch a non-list
     try:
         seq = list(seq)
+        acount += 1
     except:
-        print('Input: not iterable.')
+        print('For input: %s\n   Input: not iterable.' % str(inp))
         trivial = True
-        return trivial, None
+        acount += 1
+        return trivial, None, acount, ccount
 
     # catch non-finite data
     try:
+        ccount += len(seq) # for below, isn't "in" like a bunch of "==" statements?
         if False in np.isfinite(seq):
-            print('Input: sequence has invalid object type(s).')
+            print('For input: %s\n   Input: sequence has invalid object' \
+            ' type(s).' % str(inp))
             trivial = True
-            return trivial, None
+            acount += 1
+            return trivial, None, acount, ccount
     except:
-        print('Input: sequence has invalid object type(s).')
+        print('For input: %s\n   Input: sequence has invalid object' \
+        ' type(s).' % str(inp))
         trivial = True
-        return trivial, None
+        acount += 1
+        return trivial, None, acount, ccount
 
     # catch empty data
+    ccount += 1
     if len(seq) == 0:
-        print('Input: sequence is empty, nothing to sort.')
+        print('For input: %s\n   Input: sequence is empty, nothing ' \
+        'to sort.' % str(inp))
         trivial = True
-        return trivial, None
+        acount += 1
+        return trivial, None, acount, ccount
 
     # catch a trivial case
+    ccount += 1
     if list(set(seq)) == [seq[0]]: # detect trivial case of equal elements
         trivial = True
-        return trivial, seq
+        acount += 1
+        return trivial, seq, acount, ccount
 
-    return trivial, None
+    return trivial, None, acount, ccount
 
 def pointless_sort(x):
     """
@@ -53,7 +66,7 @@ def pointless_sort(x):
     return np.array([1,2,3])
 
 def bubblesort(seq):
-    
+
     """
     Function goes element-by-element through the list, and 
     determines if the element after it is bigger or smaller. If 
@@ -63,15 +76,33 @@ def bubblesort(seq):
     Returned list is in increasing order, smallest elements first, 
     biggest elements last.
     """
-    trivial, val = isinputcool(seq)
-    if trivial == True and val != None:
+    acount = 0 # intialize assignemnt counting
+    ccount = 0 # intialize conditional counting
+
+    inp = seq # new list obj gets redefined, no need for list comp
+    acount += 1
+
+    trivial, val, acount, ccount = isinputcool(seq, acount, ccount, inp)
+
+    ccount += 2 # for below
+    # this is in the trivial case that the seq is comprised of a single value
+    if trivial == True and val != None: 
+        print('For input: %s' % str(inp))
+        print('   Assignments: %d' % acount)
+        print('   Conditionals: %d' % ccount)
+        print('   Sequence: %s' % str(seq))
         return val
-    elif trivial == True:
-        return
+    else: # trying to count conditionals kinda precludes the use of elif, sad
+        ccount += 1
+        if trivial == True:
+            return
 
     unordered = 1 # initialize unordered
+    acount += 1
     while unordered == 1:
+        ccount += 1 # for while above
         unordered = 0 # initialize to ordered, give it a chance!
+        acount += 1 # for unordered above
         for i in range(len(seq)):
             prefix = seq[:i] # beginning part of list
             comp0 = [seq[i]] # left element for comparison
@@ -80,11 +111,19 @@ def bubblesort(seq):
                 suffix = seq[(i+2):] # end part of list
             except: # you've reached the end of the list, leave the for loop
                 break
+            ccount += 1 # for if below
             if comp0[0] > comp1[0]: # simple comparison
                 unordered = 1 # the list is unordered, cycle through again
                 # make new list in modified order:
                 seq = np.concatenate((prefix,comp1,comp0,suffix))
-    return seq
+                acount += 2
+            acount += 5 # 4 for explicit assignemnts + 1 for the implicit "i="
+    ccount += 1 # for edge while loop evaluation
+    print('For input: %s' % str(inp))
+    print('   Assignments: %d' % acount)
+    print('   Conditionals: %d' % ccount)
+    print('   Sequence: %s' % str(seq))
+    return acount, ccount
 
 def quicksort(seq,lo=0,hi='whole'):
     """
@@ -103,32 +142,59 @@ def quicksort(seq,lo=0,hi='whole'):
     portions of the sequence (left and right of the pivot) until the sequence
     is of length 1, which is by definition ordered.
     """
-    trivial, val = isinputcool(seq)
-    if trivial == True and val != None:
-        return val
-    elif trivial == True:
-        return
+    acount = 0 # intialize assignemnt counting
+    ccount = 0 # intialize conditional counting
 
+    inp = [i for i in seq] # need list comp for mutable object type 'list'
+    acount += 1
+
+    # Pre-processing start
+    trivial, val, acount, ccount = isinputcool(seq, acount, ccount, inp)
+    acount += 2 # above
+
+    ccount += 2 # below
+    if trivial == True and val != None:
+        print('For input: %s' % str(inp))
+        print('   Assignments: %d' % acount)
+        print('   Conditionals: %d' % ccount)
+        print('   Sequence: %s' % str(seq))
+        return val
+    else:
+        ccount += 1 # below
+        if trivial == True:
+            return
+
+    ccount += 1 # below
     if hi == 'whole': # extract the index of the last element
         hi = len(seq) - 1
+        acount += 1 # above
+    # Pre-processing end 
+    
+    ccount += 3 # Pre-processing
+    acount += 2 # Pre-processing
+
 
     '''
     Need to nest in a larger function so that I can use input data quality checks,
     else checks will be run on every recursive call to function.
     '''
-    def quicksorter(seq,lo,hi): 
+    def quicksorter(seq,lo,hi,acount,ccount): 
         '''
         Define the partition function that will place the pivot in the correct
         position and *partition* the function into two smaller sequences
         '''
-        def partition(seq,lo,hi):
+        def partition(seq,lo,hi,acount,ccount):
             pivot = seq[lo] # define pivot
             i = lo + 1 # left cursor
             j = hi # right cursor
             while True: # do until break when j < i (collision)
                 while i <= j and seq[i] <= pivot:
+                    ccount += 2 # for above conditionals
+                    acount += 1 # for below assignment
                     i += 1 # inch up
                 while seq[j] >= pivot and j >= i:
+                    ccount += 2 # for above conditionals
+                    acount += 1 # for below assignment
                     j -= 1 # inch down
                 if j < i:
                     break
@@ -136,16 +202,27 @@ def quicksort(seq,lo=0,hi='whole'):
                     k = seq[i]
                     seq[i] = seq[j] # do the switch
                     seq[j] = k
+                    acount += 3
+                ccount += 3 # for final edge case of while loops above
             k = seq[lo] # same storing for k above to swtich
             seq[lo] = seq[j]
             seq[j] = k
-            return j # j becomes the cusp of the new sequences
+            acount += 6 # for assingments outside while True loop
+            ccount += 1 # for while True loop
+            return j, acount, ccount # j becomes the cusp of the new sequences
         if lo < hi: # stop recursion
-            pivot = partition(seq,lo,hi)
-            quicksorter(seq,lo,pivot-1) #recursive calls of subsequence
-            quicksorter(seq,pivot+1,hi)
-        return seq
-    return quicksorter(seq,lo,hi) # gotta return something
+            acount += 1 # for assignment below
+            pivot, acount, ccount = partition(seq,lo,hi,acount,ccount)
+            quicksorter(seq,lo,pivot-1,acount,ccount) #recursive calls of subsequence
+            quicksorter(seq,pivot+1,hi,acount,ccount)
+        ccount += 1 # for above conditional
+        return seq, acount, ccount
+    seq, acount, ccount = quicksorter(seq,lo,hi,acount,ccount) # gotta return something
+    print('For input: %s' % str(inp))
+    print('   Assignments: %d' % acount)
+    print('   Conditionals: %d' % ccount)
+    print('   Sequence: %s' % str(seq))
+    return acount, ccount
 
 
 
--- test/test_algs.py
+++ test/test_algs.py
@@ -1,20 +1,27 @@
 import numpy as np
 from example import algs
-def test_pointless_sort():
-    # generate random vector of length 10
-    x = np.random.rand(10)
+# from tqdm import tqdm
+# from matplotlib import pyplot as plt
 
-    # check that pointless_sort always returns [1,2,3]
-    assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
+# from matplotlib import pyplot as plt
+# def test_pointless_sort():
+#     # generate random vector of length 10
+#     x = np.random.rand(10)
 
-    # generate a new random vector of length 10
-    x = np.random.rand(10)
+#     # check that pointless_sort always returns [1,2,3]
+#     assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
 
-    # check that pointless_sort still returns [1,2,3]
-    assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
+#     # generate a new random vector of length 10
+#     x = np.random.rand(10)
+
+#     # check that pointless_sort still returns [1,2,3]
+#     assert np.array_equal(algs.pointless_sort(x), np.array([1,2,3]))
 
 def test_bubblesort():
-    # input testing
+    print('\n############Bubblesort############')
+
+
+    # # multi test for inputs
     x = [np.array([]),
     np.array([2]),
     np.array(['a']),
@@ -22,20 +29,38 @@ def test_bubblesort():
     np.array([2,2,'a',2]),
     np.array([2,2,6,4,5]),
     np.array([2.4,2.1,6.7,4.7,5.0,10.1])]
+
+    # # # single large test
+    # x = [np.random.randint(0,10000,10000)]
+
+    # multi large test
+    # x = [np.random.rand(100,i) for i in [(i + 1) * 100 for i in list(range(10))]]
+
+    # multi smallish test
+    # x = [np.random.rand(i) for i in [(i + 1) * 10 for i in list(range(50))]]
+    #test
+    accounts = []
     for i in x:
-        ans = algs.bubblesort(i)
-        if 'None' in str(ans):
-            None
+        if i.ndim == 1:
+            ans = algs.bubblesort(i)
+            accounts.append((len(i),ans))
+        elif i.ndim == 2:
+            for j in i:
+                ans = algs.bubblesort(j)
+                accounts.append((len(j),ans))
         else:
-            print(ans)
+            None
+
     # speed testing
     # for i in [(i + 1) * 100 for i in list(range(10))]:
     #     a = np.random.rand(100,i)
     #     algs.bubblesort(a)
-    return 
+
+    return accounts
 
 def test_quicksort():
-
+    print('\n############Quicksort############')
+# # multi test for inputs
     x = [np.array([]),
     np.array([2]),
     np.array(['a']),
@@ -43,13 +68,60 @@ def test_quicksort():
     np.array([2,2,'a',2]),
     np.array([2,2,6,4,5]),
     np.array([2.4,2.1,6.7,4.7,5.0,10.1])]
-    for i in x:
-        ans = algs.bubblesort(i)
-        if 'None' in str(ans):
-            None
-        else:
-            print(ans)
-    return 
 
-test_bubblesort()
-test_quicksort()
+    # # # single large test
+    # x = [np.random.randint(0,10000,10000)]
+
+    # multi large test
+    # x = [np.random.rand(100,i) for i in [(i + 1) * 100 for i in list(range(10))]]
+
+    # multi smallish test
+    # x = [np.random.rand(i) for i in [(i + 1) * 10 for i in list(range(50))]]
+    #test
+    accounts = []
+    for i in x:
+        if i.ndim == 1:
+            ans = algs.quicksort(i)
+            accounts.append((len(i),ans))
+        elif i.ndim == 2:
+            for j in i:
+                ans = algs.quicksort(j)
+                accounts.append((len(j),ans))
+        else:
+            None
+
+    # speed testing
+    # for i in [(i + 1) * 100 for i in list(range(10))]:
+    #     a = np.random.rand(100,i)
+    #     algs.bubblesort(a)
+
+    return accounts
+
+# bub = test_bubblesort()
+# qui = test_quicksort()
+
+# plt.plot(
+#     # [i[0] for i in bub],
+#     # [i[1][0] for i in bub],
+#     # 'b',
+#     # [i[0] for i in bub],
+#     # [i[1][1] for i in bub],
+#     # 'b--')
+#     [i[0] for i in qui],
+#     [i[1][0] for i in qui],
+#     'g',
+#     [i[0] for i in qui],
+#     [i[1][1] for i in qui],
+#     'g--')
+# plt.xlabel('Length of sequence')
+# plt.ylabel('Number of conditionals/assignments')
+# plt.title('Assignments and Conditionals Scale with Input Size')
+# plt.grid(True)
+# plt.legend((#'Bubble: Assignments',
+#             #'Bubble: Conditionals'
+#             'Quick: Assignments',
+#             'Quick: Conditionals'))
+# plt.show()
+
+
+
